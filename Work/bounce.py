84548562cb9ca790e3b1b5afa9bee8fb9d1b1bae
lolPirate <debamalyadawn21@gmail.com>
2020-05-29 23:23:48 +0530

    completed exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -2,8 +2,8 @@
 #
 # Exercise 1.5
 
-height = 100 #meters
+height = 100  # meters
 
-for i in range(1,11):
+for i in range(1, 11):
     height = (3/5) * height
-    print (f'{i} {round(height, 4)}')
+    print(f'{i} {round(height, 4)}')
