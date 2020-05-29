0aaddcaff8e4005e038c61bb78ceb46bbab481c0
lolPirate <debamalyadawn21@gmail.com>
2020-05-30 01:39:08 +0530

    completed exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,15 @@
 # pcost.py
 #
 # Exercise 1.27
+
+file = r'Work\Data\portfolio.csv'
+
+total_cost = 0.0
+
+with open(file, 'rt') as f:
+    next(f)
+    for line in f:
+        name, shares, price = line.strip().split(',')
+        total_cost = total_cost + int(shares) * float(price)
+
+print(f'Total cost {total_cost:0.2f}')
