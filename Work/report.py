d681f0708a3db1bb23ccd50ec3c0854936d8d80f
lolPirate <debamalyadawn21@gmail.com>
2020-05-30 03:47:00 +0530

    completed exercise 2.12

--- Work/report.py
+++ Work/report.py
@@ -38,17 +38,50 @@ def read_prices(file_name):
     return prices
 
 
-portfolio = read_portfolio(r'Work\Data\portfolio.csv')
-current_prices = read_prices(r'Work\Data\prices.csv')
+def make_report(portfolio, current_prices):
+    """
+    Makes a report from the supplied portfolio and current_prices
+    """
+    report = []
+    for i in portfolio:
+        name, shares, price = i.values()
+        current_price = current_prices[name]
+        change = current_price - price
+        report.append((name, shares, current_price, change))
+    return report
 
-portfolio_price = 0.0
-current_price = 0.0
 
-for i in portfolio:
-    portfolio_price += i['shares'] * i['price']
-    current_price += i['shares'] * current_prices[i['name']]
+def retire(portfolio, current_prices):
+    portfolio_price = 0.0
+    current_price = 0.0
 
-print(f'Original Value : {portfolio_price:0.2f}')
-print(f'Current Value : {current_price:0.2f}')
-print(
-    f'Gain/Loss : {(current_price - portfolio_price)*100/portfolio_price:0.2f}%')
+    for i in portfolio:
+        portfolio_price += i['shares'] * i['price']
+        current_price += i['shares'] * current_prices[i['name']]
+
+    print(f'Original Value : {portfolio_price:0.2f}')
+    print(f'Current Value : {current_price:0.2f}')
+    print(
+        f'Gain/Loss : {(current_price - portfolio_price)*100/portfolio_price:0.2f}%')
+
+
+def print_report():
+    portfolio = read_portfolio(r'Work\Data\portfolio.csv')
+    current_prices = read_prices(r'Work\Data\prices.csv')
+
+    report = make_report(portfolio, current_prices)
+
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print(
+        f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
+
+    seperators = '-'*10
+    print(f'{seperators} {seperators} {seperators} {seperators}')
+
+    for name, shares, current_price, change in report:
+        current_price = round(current_price, 2)
+        current_price = '$'+str(current_price)
+        print(f'{name:>10s} {shares:>10n} {current_price:>10s} {change:>10.2f}')
+
+
+print_report()
